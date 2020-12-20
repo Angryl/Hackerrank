472786699d137f6c620ac70b7f734d2f455e578f
Rudra Pratap Singh <30315987+Angryl@users.noreply.github.com>
2020-12-20 20:47:16 +0530

    Update and rename "Hello World!" in C to "Hello World!" in C.c
    adding the solution of the prolem

---
+++ c/"Hello World!" in C.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h> 
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	  /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+  	
+   
+    return 0;
+}
